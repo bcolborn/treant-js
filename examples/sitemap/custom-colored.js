81ccd0742de81e5c797f7591ddbc3945d79dcb26
Ben Colborn <ben@nutanix.com>
2017-03-24 16:43:20 -0700

    recursion for list

--- examples/sitemap/custom-colored.js
+++ examples/sitemap/custom-colored.js
@@ -16,36 +16,75 @@ var chart_config = {
     }
 };
 
-//chart_config.nodeStructure =
-list2obj(document.getElementById("nodeStructure"), 0);
-var list =[];
-//chart_config.nodeStructure = getNestedChildren(list, document.getElementById("nodeStructure"));
+var obj = list2obj(document.getElementById("nodeStructure"));
+console.log(obj);
 
+function list2obj(x) {
 
-function getNestedChildren(arr, parent) {
-    var out =[]
-    for (var i in arr) {
-        if (arr[i].parent == parent) {
-            var children = getNestedChildren(arr, arr[i].id)
-            
-            if (children.length) {
-                arr[i].children = children
-            }
-            out.push(arr[i])
-        }
-    }
-    console.log(out);
-    return out;
-}
-function list2obj(current, depth) {
-    //console.log(current);
-    var children = current.childNodes;
-    for (var i = 0, len = children.length; i < len; i++) {
-        list2obj(children[i], depth + 1);
-    }
-    if (current.nodeName == '#text' && current.wholeText.match(/\w+/)) {
-        console.log(depth);
-        console.log(current);
-        return (current);
-    }
+	//
+	//  Output function.
+	//
+	var o = {
+		_text: ''
+	};
+
+	//
+	//  c = each child
+	//
+	var c = x.firstChild;
+
+	//
+	//  Loop through child nodes.
+	//
+	while (c) {
+
+		//
+		//  Add to _text property if it is a text.
+		//
+		if (c.nodeName == '#text') {
+			o._text += c.nodeValue;
+
+		//
+		//  Else if it is a normal node.
+		//
+		} else if (c.nodeType == 1) {
+
+			//
+			//  Create property for each node name.
+			//
+			if (typeof o[c.nodeName] == 'undefined')
+				o[c.nodeName] = [];
+			o[c.nodeName][o[c.nodeName].length] = list2obj(c);
+
+		}
+
+		//
+		//  ...Next!
+		//
+		c = c.nextSibling;
+
+	}
+
+	//
+	//  Now, attributes!
+	//
+	var a = x.attributes;
+	var i = 0;
+	if (a) {
+		for (var i = 0; i < a.length; i ++) {
+			o[a[i].name] = a[i].value;
+		}
+	}
+
+	//
+	//  Clean the text
+	//
+	if (o._text.match(/^\\s*$/))
+		delete o._text;
+
+	//
+	//  Finished!
+	//
+	return o;
+
 }
