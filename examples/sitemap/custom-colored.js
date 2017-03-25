1f80ee92597ff4b85fda9e26a8d99866c85a261d
Ben Colborn <ben@nutanix.com>
2017-03-24 17:07:49 -0700

    turn nested lists into object

--- examples/sitemap/custom-colored.js
+++ examples/sitemap/custom-colored.js
@@ -17,74 +17,50 @@ var chart_config = {
 };
 
 var obj = list2obj(document.getElementById("nodeStructure"));
-console.log(obj);
+console.log(JSON.stringify(obj, null, 2));
 
 function list2obj(x) {
-
-	//
-	//  Output function.
-	//
-	var o = {
-		_text: ''
-	};
-
-	//
-	//  c = each child
-	//
-	var c = x.firstChild;
-
-	//
-	//  Loop through child nodes.
-	//
-	while (c) {
-
-		//
-		//  Add to _text property if it is a text.
-		//
-		if (c.nodeName == '#text') {
-			o._text += c.nodeValue;
-
-		//
-		//  Else if it is a normal node.
-		//
-		} else if (c.nodeType == 1) {
-
-			//
-			//  Create property for each node name.
-			//
-			if (typeof o[c.nodeName] == 'undefined')
-				o[c.nodeName] = [];
-			o[c.nodeName][o[c.nodeName].length] = list2obj(c);
-
-		}
-
-		//
-		//  ...Next!
-		//
-		c = c.nextSibling;
-
-	}
-
-	//
-	//  Now, attributes!
-	//
-	var a = x.attributes;
-	var i = 0;
-	if (a) {
-		for (var i = 0; i < a.length; i ++) {
-			o[a[i].name] = a[i].value;
-		}
-	}
-
-	//
-	//  Clean the text
-	//
-	if (o._text.match(/^\\s*$/))
-		delete o._text;
-
-	//
-	//  Finished!
-	//
-	return o;
-
+    
+    //  Output function.
+    var o = {
+        text: {
+            name: ''
+        }
+    };
+    
+    //  c = each child
+    var c = x.firstChild;
+    
+    //  Loop through child nodes.
+    while (c) {
+        
+        //  Add to text property if it is a text.
+        if (c.nodeName == '#text' && c.wholeText.match(/\w+/)) {
+            o.text.name += c.nodeValue;
+            
+            //  Else if it is a normal node.
+        } else if (c.nodeType == 1) {
+            
+            //  Create property for each node name.
+            if (typeof o[c.nodeName] == 'undefined')
+            o[c.nodeName] =[];
+            o[c.nodeName][o[c.nodeName].length] = list2obj(c);
+        }
+        
+        //  ...Next!
+        c = c.nextSibling;
+    }
+    
+    //  Now, attributes!
+    var a = x.attributes;
+    var i = 0;
+    if (a) {
+        for (var i = 0; i < a.length; i++) {
+            o[a[i].name] = a[i].value;
+        }
+    }
+    
+    //  Finished!
+    //console.log(o.text.name);
+    return o;
 }
